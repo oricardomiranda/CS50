3d8a508c4ac0152af1b418c56e2b119b4c97b9ca
oricardomiranda <88597113+oricardomiranda@users.noreply.github.com>
2023-10-13 01:34:24 +0100

    Python W7 lecture

--- CS50P Python/Sem nome/Lecture/house.py
+++
@@ -1,4 +0,0 @@
-with open("house.csv") as file:
-	for line in file:
-		row = line.rstrip().split(",") # normal to name rows and columns
-		print(f"{row[0]} is in {row[1]}")
--- CS50P Python/Sem nome/Lecture/names3.py
+++
@@ -1,7 +0,0 @@
-name = input("What's your name? ")
-
-# open is the tool to access memory like double click
-# best way to open and close the file automatically
-with open("names.txt", "a") as file:
-	file.write(f"{name}\n")
-
