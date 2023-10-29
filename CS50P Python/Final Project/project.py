1dc210ec1e492e22178d801e812edd81b4d24d28
oricardomiranda <88597113+oricardomiranda@users.noreply.github.com>
2023-10-29 15:05:41 +0000

    Final Project

--- CS50P Python/Final Project/project.py
+++ CS50P Python/Final Project/project.py
@@ -8,7 +8,7 @@ def main():
     running = True
 
     while running:
-        action = get_action()
+        action = main_menu()
 
         if action == "C":
             # Implement the action for checking status
@@ -25,7 +25,7 @@ def main():
             running = False
             print("Goodbye!")
 
-def get_action():
+def main_menu():
     instructions = [
         {"Key": "C", "Action": "Check Status"},
         {"Key": "S", "Action": "Salaries"},
@@ -65,31 +65,16 @@ def family():
 
         if family_action == "A":
             # Code for adding a new family member
-            print("Registering a new family member")
-            first_name = input("Enter first name: ")
-            last_name = input("Enter last name: ")
-            salary = input("Enter salary: ")
-            entry_date = date.today()
-
-            with open(csv_file, mode='r') as file:
-                reader = csv.reader(file)
-                next(reader)
-                entry_id = sum(1 for row in reader) + 1
-
-            with open(csv_file, mode='a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow([entry_id, first_name, last_name, salary, entry_date])
-
-            print("New family member was created!\n")
+            add_family_member(csv_file)
         elif family_action == "E":
             # Code for editing a family member's data
-            pass
+            edit_family_member(csv_file)
         elif family_action == "F":
             # Code for checking a family member's financial data
-            pass
+            check_family_member(csv_file)
         elif family_action == "D":
             # Code for deleting a family member
-            pass
+            delete_family_member(csv_file)
         elif family_action == "Q":
             # Return False to indicate that the user wants to quit the family submenu
             return True
@@ -98,8 +83,124 @@ def family():
     # After the while loop, you can return True to continue running the main menu
     return True
 
-def function_n():
-    pass
+
+def add_family_member(csv_file):
+    print("Registering a new family member")
+    first_name = input("Enter first name: ")
+    last_name = input("Enter last name: ")
+    salary = input("Enter salary: ")
+    entry_date = date.today()
+
+    # Check if the file exists or is empty
+    if not os.path.exists(csv_file) or os.path.getsize(csv_file) == 0:
+        with open(csv_file, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["ID", "First Name", "Last Name", "Salary", "Entry Date"])
+        entry_id = 1
+    else:
+        with open(csv_file, mode='r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            entry_id = sum(1 for row in reader) + 1
+
+    with open(csv_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([entry_id, first_name, last_name, salary, entry_date])
+
+    print("New family member was created!\n")
+
+
+def edit_family_member(csv_file):
+    with open(csv_file, mode='r') as file:
+        reader = list(csv.DictReader(file))
+        # Checking if we have family members
+        if not reader:
+            print("There are no family members")
+            return
+
+        selected_member_id = select_family_member_id(reader)
+
+
+        if select_family_member_id == 0:
+            print("Update canceled.")
+            return
+
+        selected_member = reader[selected_member_id - 1]
+
+        updated_first_name = input(f"Enter new first name for {selected_member['First Name']}: ")
+        updated_last_name = input(f"Enter new last name for {selected_member['Last Name']}: ")
+        updated_salary = input(f"Enter new salary for {selected_member['Salary']}: ")
+
+        selected_member['First Name'] = updated_first_name
+        selected_member['Last Name'] = updated_last_name
+        selected_member['Salary'] = updated_salary
+
+        with open(csv_file, mode='w', newline='') as file:
+            fieldnames = ["ID", "First Name", "Last Name", "Salary", "Entry Date"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(reader)
+
+        print("Family member updated.")
+
+def check_family_member(csv_file):
+    with open(csv_file, mode='r') as file:
+        reader = list(csv.DictReader(file))
+        # Checking if we have family members
+        if not reader:
+            print("There are no family members")
+            return
+
+        selected_member_id = select_family_member_id(reader)
+
+        if select_family_member_id == 0:
+            print("Check canceled.")
+            return
+
+        selected_member = reader[selected_member_id - 1]
+
+        print(tabulate([selected_member], headers="keys", tablefmt="rounded_outline"))
+        return
+
+
+"""def delete_family_member(csv_file):
+    with open(csv_file, mode='r') as file:
+    reader = list(csv.DictReader(file))
+    # Checking if we have family members
+    if not reader:
+        print("There are no family members")
+        return
+
+    selected_member_id = select_family_member_id(reader)
+
+    if select_family_member_id == 0:
+        print("Check canceled.")
+        return
+
+    selected_member = reader[selected_member_id - 1]
+
+    print("Selected family member to delete:")
+    print(tabulate([selected_member], headers="keys", tablefmt="rounded_outline"))
+
+    confirmation =
+    return"""
+
+
+def select_family_member_id(family_data):
+    print("Select a family member:")
+    print(tabulate(family_data, headers="keys", tablefmt="rounded_outline"))
+
+    while True:
+        try:
+            selection = int(input("Select a member's ID (or 0 to cancel): "))
+            if 0 <= selection <= len(family_data):
+                return selection
+            print("Invalid ID. Please select a valid ID or 0 to cancel.")
+        except ValueError:
+            print("Invalid ID. Please enter a number or 0 to cancel.")
+
+    return 0
+
 
 if __name__ == "__main__":
     main()
