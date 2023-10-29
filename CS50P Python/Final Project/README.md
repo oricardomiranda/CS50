caf2206f01f130ab31784a640352366d2cc2f9c0
oricardomiranda <88597113+oricardomiranda@users.noreply.github.com>
2023-10-29 15:05:07 +0000

    Final Project

--- CS50P Python/Final Project/README.md
+++ CS50P Python/Final Project/README.md
@@ -1,9 +1,25 @@
 Creating an entire project may seem daunting. Here are some questions that you should think about as you start:
 
 What will your software do? What features will it have? How will it be executed?
+The software will take a family income, the monthly expenses and make calculations so that each member contributes in a fair manner.
+Features: 
+1 - Will save the name and salary of each family member
+2 - Will reserve 10% of each member for savings
+3 - Will save all fixed monthly expenses
+4 - Will calculate how much each member has to contribute, taking the person's salary in consideration
+
+Execution:
+1 - Double click to start or python command
+2 - Print instructions to guide the person. 1 - Show Earnings and savings 2 - Show expenses 3 - Add expenses 4 - Remove expenses
 What new skills will you need to acquire? What topics will you need to research?
+DB manipulation?
+How to show data in table in terminal. SQL queries?
 If working with one or two classmates, who will do what?
+N/A
 In the world of software, most everything takes longer to implement than you expect. And so it’s not uncommon to accomplish less in a fixed amount of time than you hope. What might you consider to be a good outcome for your project? A better outcome? The best outcome?
+1 - Essential: Saving expenses and salaries and divide equally
+2 - Very desired: Update salaries and expenses and divide by %
+3 - GTH: Interaction menu and some way to show the data in table. sql query?
 Consider making goal milestones to keep you on track.
 
 
--- CS50P Python/Final Project/project.py
+++ CS50P Python/Final Project/project.py
@@ -1,18 +1,105 @@
+from tabulate import tabulate
+import csv
+import os
+from datetime import date
+
 def main():
-    ...
+    print("\nFamily Expense Manager\n")
+    running = True
 
+    while running:
+        action = get_action()
 
-def function_1():
-    ...
+        if action == "C":
+            # Implement the action for checking status
+            pass
+        elif action == "S":
+            # Implement the action for managing salaries
+            pass
+        elif action == "E":
+            # Implement the action for tracking expenses
+            pass
+        elif action == "F":
+            running = family()  # Check if the user wants to continue
+        else:
+            running = False
+            print("Goodbye!")
 
+def get_action():
+    instructions = [
+        {"Key": "C", "Action": "Check Status"},
+        {"Key": "S", "Action": "Salaries"},
+        {"Key": "E", "Action": "Expenses"},
+        {"Key": "F", "Action": "Family"},
+        {"Key": "Q", "Action": "Quit"}
+    ]
+    return get_user_choice(instructions)
 
-def function_2():
-    ...
+def get_user_choice(menu):
+    while True:
+        print(tabulate(menu, headers="keys", tablefmt="rounded_outline"))
+        choice = input("Choose one action to proceed: ").strip().upper()
+        if choice in [item["Key"] for item in menu]:
+            return choice
+        print("Invalid action, try again.")
 
+def family():
+    instructions = [
+        {"Key": "A", "Action": "Add new family member"},
+        {"Key": "E", "Action": "Edit a family member's data"},
+        {"Key": "F", "Action": "Check a family member's financial data"},
+        {"Key": "D", "Action": "Delete a family member"},
+        {"Key": "Q", "Action": "Quit to Main Menu"}
+    ]
+
+    csv_file = "family.csv"
+
+    if not os.path.exists(csv_file):
+        with open(csv_file, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["ID", "First Name", "Last Name", "Salary", "Entry Date"])
+
+    while True:
+        print(tabulate(instructions, headers="keys", tablefmt="rounded_outline"))
+        family_action = input("Choose one action to proceed: ").strip().upper()
+
+        if family_action == "A":
+            # Code for adding a new family member
+            print("Registering a new family member")
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            salary = input("Enter salary: ")
+            entry_date = date.today()
+
+            with open(csv_file, mode='r') as file:
+                reader = csv.reader(file)
+                next(reader)
+                entry_id = sum(1 for row in reader) + 1
+
+            with open(csv_file, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([entry_id, first_name, last_name, salary, entry_date])
+
+            print("New family member was created!\n")
+        elif family_action == "E":
+            # Code for editing a family member's data
+            pass
+        elif family_action == "F":
+            # Code for checking a family member's financial data
+            pass
+        elif family_action == "D":
+            # Code for deleting a family member
+            pass
+        elif family_action == "Q":
+            # Return False to indicate that the user wants to quit the family submenu
+            return True
+        else:
+            print("Invalid action, try again.")
+    # After the while loop, you can return True to continue running the main menu
+    return True
 
 def function_n():
-    ...
-
+    pass
 
 if __name__ == "__main__":
     main()
